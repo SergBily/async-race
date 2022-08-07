c350080a4f2a773a2074a22762c829a0364be1e8
SergBily <sergBily.dev@gmail.com>
2022-08-07 14:10:29 +0300

    fix: load data from server

--- async-race/src/components/controller/loader.ts
+++ async-race/src/components/controller/loader.ts
@@ -1,5 +1,4 @@
 import { QueryOptions } from "../../types/interface";
-import { ResponseHeader } from "../../types/types";
 
 export class Loader {
   private baseUrl: string;
@@ -8,23 +7,13 @@ export class Loader {
     this.baseUrl = "http://127.0.0.1:3000";
   }
 
-  protected getResponse<T>(
-    options: QueryOptions,
-    endpoint: string,
-    callback: (cars: T) => void,
-    callbackTotalCars: (card: string, method?: string) => void,
-    queryParams: string[] = []
-  ): void {
-    this.load(options, endpoint, callback, callbackTotalCars, queryParams);
-  }
-
   private errorHandler(res: Response): Response {
-    if (!res.ok) throw Error(res.statusText);
+    if (!res.ok || res.status !== 304) throw Error(res.statusText);
     console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
     return res;
   }
 
-  makeUrl(endpoint: string, queryParams: string[] = []): string {
+  private makeUrl(endpoint: string, queryParams: string[] = []): string {
     let url = `${this.baseUrl}/${endpoint}`;
     url = queryParams.reduce(
       (acc, cur, index) =>
@@ -39,36 +28,18 @@ export class Loader {
     return url;
   }
 
-  protected headerHandler(
-    method: string,
-    callback: (data: string, method?: string) => void,
-    header?: ResponseHeader
-  ): void {
-    header ? callback(method, header) : callback(method);
-  }
-
-  private async load<T>(
+  protected async load(
     options: QueryOptions,
     endpoint: string,
-    callback: (cars: T) => void,
-    callbackTotalCars: (card: string, method?: string) => void,
     queryParams: string[] = []
-  ): Promise<void> {
+  ): Promise<void | Response> {
     try {
       const response: Response = await fetch(
         this.makeUrl(endpoint, queryParams),
         options
       );
-      const header: ResponseHeader = response.headers.get("X-Total-Count");
-      const result: T = await response.json();
 
-      if (options.method === "DELETE") {
-        callback(queryParams[0]);
-      } else {
-        callback(result);
-      }
-
-      this.headerHandler(options.method, callbackTotalCars, header);
+      return response;
     } catch (err: unknown) {
       console.error(err);
     }
