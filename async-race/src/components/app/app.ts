cbbc049fc1c455cbe44228463c10b7c15d35fb77
SergBily <sergBily.dev@gmail.com>
2022-08-07 14:06:24 +0300

    fix: method start

--- async-race/src/components/app/app.ts
+++ async-race/src/components/app/app.ts
@@ -3,16 +3,22 @@ import { Appcontroller } from "../controller/controller";
 import { Garage } from "../view/garage/garage";
 
 export class App {
-  controller: Appcontroller;
+  private controller: Appcontroller;
   private readonly garage: Garage;
 
   constructor() {
     this.controller = new Appcontroller();
     this.garage = new Garage();
   }
-  public start(): void {
-    this.controller.getGaragePage((data: DataServer[]) =>
-      this.garage.createPageGarage(data)
-    );
+  public async start(): Promise<void> {
+    const response = (await this.controller.getGaragePage()) as Response;
+    const data: DataServer[] = await response.json();
+    const numPage: string = response.url.split("=")[1][0];
+    const totalCars = response.headers.get("X-Total-Count") as string;
+
+    this.garage.drawCars(data);
+    this.garage.createPageGarage();
+    this.garage.drawTotalCars("GET", totalCars);
+    this.garage.drawNumPage(numPage);
   }
 }
